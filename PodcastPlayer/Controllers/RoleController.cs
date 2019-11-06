2fc23f2c8b7cfcd045d2e03dd22ef82764346481
Grant Watson <gwatson117@gmail.com>
2019-11-06 10:57:34 -0700

    Views and Controllers Added for Identity

---
+++ PodcastPlayer/Controllers/RoleController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PodcastPlayer.Controllers
+{
+    [Authorize(Roles = Services.App.Pages.Role.RoleName)]
+    public class RoleController : Controller
+    {
+        private readonly Services.Security.ICommon _security;
+
+        //dependency injection through constructor, to directly access services
+        public RoleController(Services.Security.ICommon security)
+        {
+            _security = security;
+        }
+
+        //consume custom security service to get all roles
+        public IActionResult Index()
+        {
+            List<string> roles = new List<string>();
+            roles = _security.GetAllRoles();
+            return View(roles);
+        }
+    }
+}
---
+++ PodcastPlayer/ViewModels/ChangeRoles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PodcastPlayer.ViewModels
+{
+    //view model for changeroles screen
+    public class ChangeRoles
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public bool IsMembershipRegistered { get; set; }
+        public bool IsRoleRegistered { get; set; }
+    }
+}
